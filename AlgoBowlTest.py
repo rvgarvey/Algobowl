c0649dde8b8d208ba0c7b82567984f99a99d8811
Trevor <tlhartshorn@mymail.mines.edu>
2021-02-28 15:05:34 -0700

    Working algorithm; needs efficiency

--- AlgoBowlTest.py
+++ AlgoBowlTest.py
@@ -2,37 +2,31 @@ f = open("AlgoBowlInputFile.txt")
 
 content = f.read()
 inputArr = content.split()
-print(inputArr)
+inputArr = [int(i) for i in inputArr]
 
 calcArr = []
 tempArr = []
-calcArr[0] = [1,1,2]
+calcArr.append([1,1,2])
 result = 0
 
-for i in range(2, len(inputArr) - 1):
-    diff = inputArr[i] - inputArr[i-1]
-    if(diff not in tempArr):
-        tempArr.append(diff)
+def calculateValue(x):
+    if(x == 1):
+        return 1
+    if(x in [addition[2] for addition in calcArr]):
+        return x
 
-tempArr.sort()
+    if(x % 2 == 0):
+        addend = calculateValue(x // 2)
+        calcArr.append([addend,addend,x])
+        return x
+    else:
+        addend2 = calculateValue(x // 2 + 1)
+        addend1 = calculateValue(x // 2)
+        calcArr.append([addend1,addend2,x])
+        return x
 
+for i in range(1, len(inputArr)):
+    calculateValue(inputArr[i])
 
-def recusionFunction(x):
-    if(x == 2):
-        return 2
-
-    recursionFunction(x += x/2)
-
-
-if(tempArr[0] != 2):
-   
-
-for tempElement in tempArr:
-    for calcElement in calcArr:
-        if calcElement[2]
-
-        if(result not in calcElemtent[:,2]):
-            calcArr.append(addend1, addend2, result)
-    
 for x in calcArr:
     print(x)
